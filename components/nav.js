971305f75fcc82775be118d128be471a43fcd906
N120 <n12o.dev@gmail.com>
2020-02-01 12:02:58 +0200

    Add page, component, global styles

---
+++ components/nav.js
@@ -0,0 +1,22 @@
+const Nav = () => {
+    return (
+        <nav>
+            <ul className={"nav"}>
+                <li>
+                    <a>Hello there</a>
+                </li>
+                <li>
+                    <a>Hello there</a>
+                </li>
+                <li>
+                    <a>Hello there</a>
+                </li>
+                <li>
+                    <a>Hello there</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Nav from '../components/nav';
+
+const Page = () => {
+  return (
+    <>
+      <Nav></Nav>
+      <h1>Hello World!</h1>
+    </>
+  );
+};
+
+export default Page;
